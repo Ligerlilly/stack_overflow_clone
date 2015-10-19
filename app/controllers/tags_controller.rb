aa9d2391b854551ab3bf9da7f8a1f6e4ef9ad353
Andrew Dang & Jason Lane <student@epicodus.com>
2015-10-19 15:25:17 -0700

    CRUD for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+  before_action :find_tag, except: [:index, :new, :create]
 
   def index
     @tags = Tag.all
@@ -13,15 +14,33 @@ class TagsController < ApplicationController
     if @tag.save
       redirect_to new_question_path
     else
-    
+
     end
   end
 
   def edit
-
   end
 
+  def update
+    if @tag.update(tag_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag.destroy
+    redirect_to questions_path
+  end
+
+
 private
+
+  def find_tag
+    @tag = Tag.find(params[:id])
+  end
+
   def tag_params
     params.require(:tag).permit(:name)
   end
